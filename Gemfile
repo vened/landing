329cb4aac6f1a495500763e9dd89c4bdf4a93628
Stolbukhin Maksim <stolbukhin@mamba.ru>
2014-08-10 17:49:12 +0400

    настройки для деплоя

--- Gemfile
+++ Gemfile
@@ -28,6 +28,17 @@ gem 'sdoc', '~> 0.4.0',        group: :doc
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',      group: :development
 
+
+group :development do
+  gem 'rvm-capistrano'
+  gem 'capistrano'
+end
+
+group :production do
+  gem 'unicorn'
+end
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ config/deploy.rb
@@ -0,0 +1,54 @@
+require "rvm/capistrano"
+require "bundler/capistrano"
+
+set :application, "landing"
+set :shared_children, shared_children
+set :repository, "git@github.com:vened/landing.git"
+set :deploy_to, "/var/www/landing"
+set :scm, :git
+set :branch, "master"
+set :user, "max"
+set :group, "staff"
+set :use_sudo, false
+set :rails_env, "production"
+set :deploy_via, :copy
+set :ssh_options, {:forward_agent => true, :port => 22}
+set :keep_releases, 5
+default_run_options[:pty] = true
+server "185.22.60.226", :app, :web, :db, :primary => true
+
+after "deploy", "deploy:cleanup"
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/production_database.yml"), "#{shared_path}/config/database.yml"
+    puts "Теперь вы можете отредактировать файлы в  #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+
+end
