2787b870db2a30c28af0e774afa0ce9294198dea
Stolbukhin Maksim <stolbukhin@mamba.ru>
2014-08-15 18:11:05 +0400

    настройки для деплоя

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -15,7 +15,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/landing/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=username
+AS_USER=max
 set -u
 
 OLD_PIN="$PID.oldbin"
